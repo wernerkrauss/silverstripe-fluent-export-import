b2017c5b23c2866c2c448d9f133926a2d199f2ce
Werner Krauß <werner.krauss@netwerkstatt.at>
2024-11-07 19:42:27 +0100

    refactor business logic to own class

---
+++ src/Helper/FluentExportHelper.php
@@ -0,0 +1,96 @@
+<?php
+
+namespace Netwerkstatt\FluentExIm\Helper;
+
+use Symfony\Component\Yaml\Yaml;
+
+class FluentExportHelper
+{
+
+    public static function exportClass(string $className, string $locale): string
+    {
+        //create data array for each record of $className with ID and translatable fields
+        $data = [];
+
+        $records = $className::get()
+            ->filter(['ClassName' => $className]);
+        if ($records->count() === 0) {
+            return '';
+        }
+
+        $data[$locale][$className] = [];
+//        $data[$locale][$className]['Info'] = [
+//            'Title' => singleton($className)->singular_name(),
+//            'Records' => $records->count()
+//        ];
+
+        foreach ($records as $record) {
+            $recordData = FluentHelper::getLocalisedDataFromDataObject($record, $locale);
+            if ($recordData === []) {
+                continue;
+            }
+
+            $data[$locale][$className][$record->ID] = $recordData;
+        }
+
+        if ($data[$locale][$className] === []) {
+            return '';
+        }
+
+        $sanitisedClassName = str_replace('\\', '-', $className);
+
+        return self::createYmlFile($sanitisedClassName, $data, $locale);
+    }
+
+    public static function createYmlFile(string $className, array $data, string $locale): string
+    {
+        $yaml = YAML::dump($data, 10, 2, YAML::DUMP_MULTI_LINE_LITERAL_BLOCK);
+
+        $dir = TEMP_PATH . '/fluent-ex-im/';
+        if (!file_exists($dir) && (!mkdir($dir, 0777, true) && !is_dir($dir))) {
+            throw new \RuntimeException(sprintf('Directory "%s" was not created', $dir));
+        }
+
+        $filename = $dir . $className . '_' . $locale . '.yml';
+        file_put_contents($filename, $yaml);
+        return $filename;
+    }
+
+    /**
+     * @return array
+     */
+    public static function exportAllFluentClasses(string $locale): array
+    {
+        //get all classes with FluentExtension
+        $classes = FluentHelper::getFluentClasses();
+        $filenames = [];
+
+        foreach ($classes as $key => $className) {
+            if (count(FluentHelper::getTranslatedFieldsForClass($className)) === 0) {
+                continue;
+            }
+
+            $filenames[$className] = FluentExportHelper::exportClass($className, $locale);
+        }
+         //remove empty entries
+        return array_filter($filenames);
+    }
+
+    /**
+     * @param array $filenames
+     * @return string
+     */
+    public static function generateZipArchive(array $filenames): string
+    {
+        $zip = new \ZipArchive();
+        $zipFilename = TEMP_PATH . '/fluent-ex-im/fluent-export.zip';
+        $zip->open($zipFilename, \ZipArchive::CREATE);
+        foreach ($filenames as $key => $filename) {
+            $zip->addFile($filename, basename((string)$filename));
+        }
+
+        $zip->close();
+
+        return $zipFilename;
+    }
+}
--- src/Task/FluentExport.php
+++ src/Task/FluentExport.php
@@ -2,11 +2,10 @@
 
 namespace Netwerkstatt\FluentExIm\Task;
 
-use Netwerkstatt\FluentExIm\Helper\FluentHelper;
+use Netwerkstatt\FluentExIm\Helper\FluentExportHelper;
 use SilverStripe\Control\Director;
 use SilverStripe\Dev\BuildTask;
 use SilverStripe\ORM\DB;
-use Symfony\Component\Yaml\Yaml;
 use TractorCow\Fluent\Model\Locale;
 
 class FluentExport extends BuildTask
@@ -40,39 +39,17 @@ class FluentExport extends BuildTask
      */
     public function run($request): void
     {
-        //get all classes with FluentExtension
-        $classes = FluentHelper::getFluentClasses();
-        $filenames = [];
+        $locale = Locale::getDefault()->Locale;
 
-        foreach ($classes as $key => $className) {
-            if (count(FluentHelper::getTranslatedFieldsForClass($className)) === 0) {
-                continue;
-            }
+        $filenames = FluentExportHelper::exportAllFluentClasses($locale);
 
-            $filenames[$className] = $this->exportClass($className);
-        }
-
-        if ($filenames !== []) {
-//            DB::alteration_message('Exported ' . count($filenames) . ' classes to yml files:');
-//            foreach ($filenames as $key => $filename) {
-//                DB::alteration_message($filename);
-//            }
-        } else {
+        if ($filenames == []) {
             DB::alteration_message('No classes with FluentExtension found');
             return;
         }
 
-        $filenames = array_filter($filenames); //remove empty entries
+        $zipFilename = FluentExportHelper::generateZipArchive($filenames);
 
-        //zip all files and offer download
-        $zip = new \ZipArchive();
-        $zipFilename = TEMP_PATH . '/fluent-ex-im/fluent-export.zip';
-        $zip->open($zipFilename, \ZipArchive::CREATE);
-        foreach ($filenames as $key => $filename) {
-            $zip->addFile($filename, basename($filename));
-        }
-
-        $zip->close();
         if (Director::is_cli()) {
             echo 'Exported ' . count($filenames) . ' classes to yml files:' . PHP_EOL;
             foreach ($filenames as $key => $filename) {
@@ -90,52 +67,4 @@ class FluentExport extends BuildTask
         readfile($zipFilename);
     }
 
-    private function exportClass(string $className): string
-    {
-        //create data array for each record of $className with ID and translatable fields
-        $data = [];
-        $locale = Locale::getDefault()->Locale;
-        $records = $className::get()
-            ->filter(['ClassName' => $className]);
-        if ($records->count() === 0) {
-            return '';
-        }
-
-        $data[$locale][$className] = [];
-//        $data[$locale][$className]['Info'] = [
-//            'Title' => singleton($className)->singular_name(),
-//            'Records' => $records->count()
-//        ];
-
-        foreach ($records as $record) {
-            $recordData = FluentHelper::getLocalisedDataFromDataObject($record, $locale);
-            if ($recordData === []) {
-                continue;
-            }
-
-            $data[$locale][$className][$record->ID] = $recordData;
-        }
-
-        if ($data[$locale][$className] === []) {
-            return '';
-        }
-
-        $sanitisedClassName = str_replace('\\', '-', $className);
-
-        return $this->createYmlFile($sanitisedClassName, $data, $locale);
-    }
-
-    private function createYmlFile(string $className, array $data, string $locale): string
-    {
-        $yaml = YAML::dump($data, 10, 2, YAML::DUMP_MULTI_LINE_LITERAL_BLOCK);
-
-        $dir = TEMP_PATH . '/fluent-ex-im/';
-        if (!file_exists($dir)) {
-            mkdir($dir, 0777, true);
-        }
-
-        $filename = $dir . $className . '_' . $locale . '.yml';
-        file_put_contents($filename, $yaml);
-        return $filename;
-    }
 }
