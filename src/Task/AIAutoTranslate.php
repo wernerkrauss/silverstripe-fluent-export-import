6fb527c655ea6abe302345b685c9fa4908d3304f
Werner Krauß <werner.krauss@netwerkstatt.at>
2025-06-27 15:04:08 +0200

    upgrade to SS6
    fix buildtasks

--- src/Task/AIAutoTranslate.php
+++ src/Task/AIAutoTranslate.php
@@ -2,9 +2,8 @@
 
 namespace Netwerkstatt\FluentExIm\Task;
 
-use RuntimeException;
-use InvalidArgumentException;
 use Exception;
+use InvalidArgumentException;
 use Monolog\Formatter\LineFormatter;
 use Monolog\Handler\FilterHandler;
 use Monolog\Handler\StreamHandler;
@@ -14,22 +13,25 @@ use Netwerkstatt\FluentExIm\Extension\AutoTranslate;
 use Netwerkstatt\FluentExIm\Helper\FluentHelper;
 use Netwerkstatt\FluentExIm\Translator\AITranslationStatus;
 use Psr\Log\LoggerInterface;
+use RuntimeException;
 use SilverStripe\Core\Injector\Injector;
 use SilverStripe\Dev\BuildTask;
 use SilverStripe\ORM\DataObject;
 use SilverStripe\PolyExecution\PolyOutput;
+use Symfony\Component\Console\Command\Command;
 use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Input\InputOption;
 use TractorCow\Fluent\Model\Locale;
 use TractorCow\Fluent\State\FluentState;
 
 class AIAutoTranslate extends BuildTask
 {
+    protected static string $commandName = 'FluentAIAutoTranslate';
+
     /**
      * @config
      */
     private static $segment = 'fluent-ai-autotranslate';
-
-
     /**
      * @config
      */
@@ -68,25 +70,24 @@ class AIAutoTranslate extends BuildTask
     protected function execute(InputInterface $input, PolyOutput $output): int
     {
         $defaultLocale = Locale::getDefault()->Locale;
-        $currentLocale = Locale::getCurrentLocale()->Locale;
-        if ($currentLocale !== $defaultLocale && $input->getOption('locale')) {
-            $defaultLocale = $input->getOption('locale');
+        $currentLocale = Locale::getCurrentLocale()?->Locale;
+
+        if ($currentLocale !== $defaultLocale && $input->hasOption('locale_from')) {
+            $defaultLocale = $input->getOption('locale_from');
             FluentState::singleton()->setLocale($defaultLocale);
             $currentLocale = Locale::getCurrentLocale()->Locale;
         }
 
         if ($currentLocale !== $defaultLocale) {
-            throw new RuntimeException('Please run this task in default locale');
+            throw new RuntimeException('Please run this task in default locale or set --locale-from to a valid locale');
         }
 
-        if ($request->getVar('do_publish') === null) {
+        if ($input->getOption('do_publish') === null) {
             throw new InvalidArgumentException('Please provide do_publish parameter. 1 will publish all translated objects, 0 will only write to stage');
         }
 
-        $doPublish = (bool) $input->getOption('do_publish');
-        $forceTranslation = (bool) $input->getOption('force_translation');
-
-
+        $doPublish = (bool)$input->getOption('do_publish');
+        $forceTranslation = (bool)$input->getOption('force_translation');
 
         $fluentClasses = FluentHelper::getFluentClasses();
         foreach ($fluentClasses as $fluentClassName) {
@@ -100,30 +101,51 @@ class AIAutoTranslate extends BuildTask
                 continue;
             }
 
-            $output->writeln('** ' . $fluentClass->singular_name() . ' **' );
+            $output->startList(); //some space between classes
+            $output->writeListItem('<options=bold,underscore>' . $fluentClass->singular_name() . '</>');
             $translatableItems = FluentState::singleton()
                 ->setLocale($defaultLocale)
                 ->withState(static fn(FluentState $state) => DataObject::get($fluentClassName));
             foreach ($translatableItems as $translatableItem) {
                 $translatableItem = $translatableItem->fixLastTranslationForDefaultLocale();
                 $status = $translatableItem->autoTranslate($doPublish, $forceTranslation);
-//                $this->outputStatus($output, $status); //@todo: fix output logging...
+                $this->outputStatus($output, $status);
             }
+            $output->stopList();
         }
+        return Command::SUCCESS;
     }
 
     private function outputStatus(PolyOutput $output, AITranslationStatus $status)
     {
-        $msg = $status->getObject()->ClassName . ': ' . $status->getObject()->getTitle() . ' (' . $status->getObject()->ID . '): ' . PHP_EOL;
+        $msg = $status->getObject()->ClassName . ': ' . $status->getObject()->getTitle() . ' (' . $status->getObject()->ID . '): ';
         $msg = $status->getMessage() !== '' && $status->getMessage() !== '0' ? $msg . ' - ' . $status->getMessage() : $msg;
-        $this->log($status->getStatus(), $msg);
 
+        $msg = match (AITranslationStatus::getLogLevel($status)) {
+            'warning' => '<comment>' . $msg . '</comment>',
+            'error' => '<error>' . $msg . '</error>',
+            default => '<info>' . $msg . '</info>',
+        };
+
+        $output->startList();
+        $output->writeListItem($msg);
         $localesTranslatedTo = $status->getLocalesTranslatedTo();
         if ($localesTranslatedTo !== []) {
+            $output->startList();
             foreach ($localesTranslatedTo as $locale => $localeStatus) {
-                $this->log(AITranslationStatus::getLogLevel($localeStatus), ' * ' . $locale . ': ' . $localeStatus);
+                $status = AITranslationStatus::getLogLevel($localeStatus);
+                $msg = $locale . ': ' . $localeStatus;
+                $msg = match ($status) {
+                    'warning' => '<comment>' . $msg . '</comment>',
+                    'error' => '<error>' . $msg . '</error>',
+                    default => '<info>' . $msg . '</info>',
+                };
+                $output->writeListItem($msg);
+//                $this->log(AITranslationStatus::getLogLevel($localeStatus), ' * ' . $locale . ': ' . $localeStatus);
             }
+            $output->stopList();
         }
+        $output->stopList();
     }
 
     /**
@@ -170,5 +192,33 @@ class AIAutoTranslate extends BuildTask
             'error' => $this->logger->error($msg),
             default => $this->logger->info($msg),
         };
+    }
+
+    public function getOptions(): array
+    {
+        return [
+            new InputOption(
+                'locale_from',
+                'l',
+                InputOption::VALUE_REQUIRED,
+                'set the locale to translate from; use the default locale if not set.',
+                Locale::getDefault()->Locale,
+                Locale::get()->column('Locale')
+            ),
+            new InputOption(
+                'do_publish',
+                'p',
+                InputOption::VALUE_REQUIRED,
+                'publish records. Must be set to 1 for this task to run',
+                null, //must be set
+                [0, 1]
+            ),
+            new InputOption(
+                'force_translation',
+                null,
+                InputOption::VALUE_NONE,
+                'Should all texts be translated again, even if they\'re already translated?'
+            )
+        ];
     }
 }
--- src/Task/FluentExport.php
+++ src/Task/FluentExport.php
@@ -7,11 +7,13 @@ use SilverStripe\Control\Director;
 use SilverStripe\Dev\BuildTask;
 use SilverStripe\ORM\DB;
 use SilverStripe\PolyExecution\PolyOutput;
+use Symfony\Component\Console\Command\Command;
 use Symfony\Component\Console\Input\InputInterface;
 use TractorCow\Fluent\Model\Locale;
 
 class FluentExport extends BuildTask
 {
+    protected static string $commandName = 'FluentExport';
     /**
      * @config
      */
@@ -46,20 +48,23 @@ class FluentExport extends BuildTask
         $filenames = FluentExportHelper::exportAllFluentClasses($locale);
 
         if ($filenames == []) {
-            $output->writeln('No classes with FluentExtension found');
+            $output->writeln('<error>No classes with FluentExtension found</error>');
             return 0;
         }
 
         $zipFilename = FluentExportHelper::generateZipArchive($filenames, $locale);
 
         if (Director::is_cli()) {
-            $output->writeln('Exported ' . count($filenames) . ' classes to yml files:');
+            $output->writeln('');
+            $output->writeln('<info>Exported ' . count($filenames) . ' classes to yml files:</info>');
+            $output->startList();
             foreach ($filenames as $key => $filename) {
-                $output->writeln($filename );
+                $output->writeListItem($filename );
             }
+            $output->stopList();
 
-            $output->writeln('Zip file created: ' . $zipFilename);
-            return 1;
+            $output->writeln('<info>Zip file created in: </info><comment>' . $zipFilename . '</comment>');
+            return Command::SUCCESS;
         }
 
         ob_clean();
@@ -67,6 +72,6 @@ class FluentExport extends BuildTask
         header('Content-Disposition: attachment; filename="' . basename($zipFilename) . '"');
         header('Content-Length: ' . filesize($zipFilename));
         readfile($zipFilename);
-        return 1;
+        return Command::SUCCESS;
     }
 }
